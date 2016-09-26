adc55d9c8ccc686ecdf0c0cb76aa3c2759aba7b1
pcimagenca <oterolopez1990@gmail.com>
2016-09-26 16:06:37 -0400

    nuevo

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/mystyle.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><i class="glyphicon glyphicon-earphone icono"> hola </i></li>
+        <li>numero 1 <br> numero 2</li>
+        <li class="bolder icono">@</li>
+        <li>Contacto</li>
+        <li><a href="#" class="btn btn-primary btn-register">registrar inmueble</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<nav class="navbar navbar-default navbar-fixed-top" style="display:none;">
+  <div class="container">
+    pegao
+  </div>
+</nav>
+
+<div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a>
+        </p>
+      </div>
+
+<div class="container-fluid"> 
+  <div class="row elementopadre text-center bolder">
+    <hr class="titulohr">  <h1>  titulo</h1>    <hr class=" titulohr">  
+    <div class="col-md-4 col-xs-12 text-center elementohijo">  <i class="glyphicon glyphicon-home  icono2"></i>
+    <h2 class="titulo"> casas</i></h1> <div class="descripcion"> </div>
+    </div>
+    <div class="col-md-4 col-xs-12 text-center elementohijo"><i class="glyphicon glyphicon-home icono2"></i><h1 class="titulo"> apartamentos</h1> <div class="descripcion"> </div></div>
+    <div class="col-md-4 col-xs-12 text-center elementohijo"><i class="glyphicon glyphicon-home icono2"></i><h1 class="titulo"> oficinas</h1> <div class="descripcion"> </div></div>
+  </div>
+  <div class="row section1"> 
+    <hr class="titulohr">  <h1 class="text-center">  titulo</h1>    <hr class=" titulohr">  
+    <h4 class=" text-center"> subtitulo descripcion subtitulo</h4>
+    <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento  </div>
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div>
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+     <div class="col-md-4 col-lg-3 col-sm-6 col-xs-12">elemento</div> 
+  </div>
+
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
